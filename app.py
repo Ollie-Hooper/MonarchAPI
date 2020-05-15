8ba0c2b576f3f67632aa625208451200ca27b59e
Ollie Hooper <35010178+Ollie-Hooper@users.noreply.github.com>
2020-05-15 19:37:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from datetime import datetime as dt
 
 data = pd.read_csv('data.csv', names=['s', 'e', 'm']).set_index('m')
 
-series = pd.Series(index=range(data.s.min(), dt.now().year))
+series = pd.Series(index=range(data.s.min(), dt.now().year + 1))
 for m in data.index:
     series.loc[data.loc[m].s:data.loc[m].e] = m
 
